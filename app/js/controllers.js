cafa92b9f5919d5afd544044aabeae9fef576eb7
Whitney O'Banner <wobanner@gmail.com>
2014-10-05 16:04:50 -0700

    Access track favorites on SoundCloud
    * Use SoundCloud API call to get top 20 favorites
    * Store favorites on scope for access in view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('HomeCtrl', [function() {
-
+  controller('HomeCtrl', ['$scope', function($scope) {
+    SC.get('/users/thunder-hustle/favorites', { limit: 20 }, function(tracks) {
+      $scope.$apply(function() {
+        if (tracks.length > 1) {
+          $scope.likes = tracks;
+        } else {
+          console.log("user has no favorites");
+        }
+      });
+    });
   }]);
